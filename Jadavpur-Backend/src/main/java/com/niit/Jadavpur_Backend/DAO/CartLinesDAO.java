d651c66b3761d7a4bef7520f779b1009640a9a67
ghosh3428 <ghosh.debabrato@gmail.com>
2023-02-21 17:29:23 +0530

    Add files via upload

---
+++ Jadavpur-Backend/src/main/java/com/niit/Jadavpur_Backend/DAO/CartLinesDAO.java
@@ -0,0 +1,18 @@
+package com.niit.Jadavpur_Backend.DAO;
+
+import java.util.List;
+
+import com.niit.Jadavpur_Backend.modal.Cart;
+import com.niit.Jadavpur_Backend.modal.CartLines;
+
+public interface CartLinesDAO 
+{
+	public List<CartLines> list(int cartId);
+	public CartLines get(int id);	
+	public boolean add(CartLines cartLine);
+	public boolean update(CartLines cartLine);
+	public boolean remove(CartLines cartLine);
+	boolean updateCart(Cart cart);
+	public List<CartLines> listAvailable(int cartId);
+
+}
